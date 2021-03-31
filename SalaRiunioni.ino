6878e179ab01297be9eb733db494852caf415cf8
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-03-31 16:43:56 +0200

    Sposta accensione del laser fuori dal loop

--- SalaRiunioni.ino
+++ SalaRiunioni.ino
@@ -1,15 +1,13 @@
-#include <Arduino.h>
-
 #define LASER_PIN 6
 #define LIGHT_PIN A3
 
 void setup() {
   pinMode(LASER_PIN, OUTPUT);
   pinMode(LIGHT_PIN, INPUT);
+  digitalWrite(LASER_PIN, HIGH);
 }
 
 void loop() {
-  digitalWrite(LASER_PIN, HIGH);
   int val = analogRead(LIGHT_PIN);
   Serial.println(val);
   delay(100);
