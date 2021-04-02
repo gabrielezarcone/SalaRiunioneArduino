aeaca0182c33cde0daa381af9c61411f74387d02
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-04-02 10:13:33 +0200

    Anche controllo uscita

--- SalaRiunioni.ino
+++ SalaRiunioni.ino
@@ -4,7 +4,7 @@
 #define LASER_PIN1 6
 #define LASER_PIN2 7
 #define DETECTOR_PIN1 A3
-#define DETECTOR_PIN2 5
+#define DETECTOR_PIN2 A5
 
 SensorePassaggio sensorePorta(LASER_PIN1, LASER_PIN2, DETECTOR_PIN1, DETECTOR_PIN2);
 Schermo lcd;
