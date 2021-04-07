02d762f068a3e2aea6be3889fb4f6c67885a0fb0
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-04-07 10:36:33 +0200

    Codice per board client wifi

---
+++ ClientBoard/Client.ino
@@ -0,0 +1,12 @@
+/*
+    Questo codice deve essere inserito all'interno della board (ESP8266) che si occuperà di effettuare le chiamate HTTP al server.
+    Riceve istruzioni dalla board principale (MEGA2560) attraversolo la comunicazione seriale.
+*/
+
+void setup(){
+    Serial.begin(9600);
+}
+
+void loop(){
+    
+}
