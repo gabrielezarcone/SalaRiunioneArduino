143b7b5235d1d069b9d7da42f12cf14bd9ca831a
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-04-09 13:50:19 +0200

    Ampliato il file di configurazione del modulo WIFI

--- ClientBoard/src/HttpService/Secrets.h
+++ ClientBoard/src/HttpService/Secrets.h
@@ -3,5 +3,7 @@
 
 #define SSID_WIFI ""
 #define PASSWORD_WIFI ""
+#define BASE_URL "http://192.168.85.47:8080"
+#define ARDUINO_NAME "arduino1"
 
 #endif
--- ClientBoard/src/SerialComunication/SerialComunication.cpp
+++ ClientBoard/src/SerialComunication/SerialComunication.cpp
@@ -18,13 +18,21 @@ void SerialComunication::checkMainBoard(){
 
 
 void SerialComunication::httpGetPrenotazioneAttuale(){
-    http.sendRequest("GET", "http://192.168.1.136:8050/prenotazione/findPrenotazioneAttuale/arduino1");
+    char* url;
+    strcpy(url, BASE_URL);
+    strcat(url, "/prenotazione/findPrenotazioneAttuale/");
+    strcat(url, ARDUINO_NAME);
+    http.sendRequest("GET", url);
     http.request.onReadyStateChange(SerialComunication::_handlerGetPrenotazioneAttuale);    // TODO da mandare la risposta alla board principale gestendo il JSON
 }
 
 
 void SerialComunication::httpGetPrenotazioneSuccessiva(){
-    http.sendRequest("GET", "http://192.168.1.136:8050/prenotazione/findPrenotazioneSuccessiva/arduino1");
+    char* url;
+    strcpy(url, BASE_URL);
+    strcat(url, "/prenotazione/findPrenotazioneSuccessiva/");
+    strcat(url, ARDUINO_NAME);
+    http.sendRequest("GET", url);
     http.request.onReadyStateChange(SerialComunication::_handlerGetPrenotazioneSuccessiva);    // TODO da mandare la risposta alla board principale gestendo il JSON
 }
 
@@ -47,8 +55,10 @@ void SerialComunication::httpPostCounter(){
             doc["arduinoID"] = "arduino1";
             serializeJson(doc, body);
             
-            Serial.println(body);
-            http.sendRequest("POST", "http://192.168.1.136:8050/stanza/counter", &body[0]); //inserisce anche il body facoltativo come array di caratteri
+            char* url;
+            strcpy(url, BASE_URL);
+            strcat(url, "/stanza/counter");
+            http.sendRequest("POST", url, &body[0]); //inserisce anche il body facoltativo come array di caratteri
             return;
         }
         yield(); // da mettere su ogni while quando si usa ESP8266
@@ -67,7 +77,10 @@ void SerialComunication::httpPostTemperature(){
             doc["arduinoID"] = "arduino1";
             serializeJson(doc, body);
             
-            http.sendRequest("POST", "http://192.168.1.136:8050/stanza/temperaturaStanza", &body[0]); //inserisce anche il body facoltativo come array di caratteri
+            char* url;
+            strcpy(url, BASE_URL);
+            strcat(url, "/stanza/temperaturaStanza");
+            http.sendRequest("POST", url, &body[0]); //inserisce anche il body facoltativo come array di caratteri
             return;
         }
         yield();
